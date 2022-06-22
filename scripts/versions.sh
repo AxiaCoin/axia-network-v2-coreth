48aa2c5abc7ea6c459aa9752b019cd8dd7a5309c
sankar.boro <sankar.boro@zeeve.io>
2022-06-22 19:06:37 +0530

    preparing v0.1.14

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Set up the versions to be used
-coreth_version=${CORETH_VERSION:-'v0.1.2'}
+coreth_version=${CORETH_VERSION:-'v0.1.14'}
 # Don't export them as they're used in the context of other calls
-axia_version=${AXIA_VERSION:-'v0.1.2'}
+axia_version=${AXIA_VERSION:-'v0.1.14'}
