80aa548e828e345bd8e12e4d0061d229a0173380
sankar.boro <sankar.boro@zeeve.io>
2022-06-28 13:21:52 +0530

    update axia

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.17
 
 require (
 	github.com/VictoriaMetrics/fastcache v1.9.0
-	github.com/axiacoin/axia-network-v2 v0.0.0-20220628071156-93755b2ca420
+	github.com/axiacoin/axia-network-v2 v0.0.0-20220628074935-083407329de0
 	github.com/cespare/cp v0.1.0
 	github.com/davecgh/go-spew v1.1.1
 	github.com/deckarep/golang-set v1.8.0
--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Set up the versions to be used
-coreth_version=${CORETH_VERSION:-'v0.0.0-20220628071156-93755b2ca420'}
+coreth_version=${CORETH_VERSION:-'v0.0.0-20220628074935-083407329de0'}
 # Don't export them as they're used in the context of other calls
-axia_version=${AXIA_VERSION:-'v0.0.0-20220628071156-93755b2ca420'}
+axia_version=${AXIA_VERSION:-'v0.0.0-20220628074935-083407329de0'}
