c809b0928ed2a51092bc47020255c0b0441fb1e5
sankar.boro <sankar.boro@zeeve.io>
2022-07-13 20:30:35 +0530

    new genesis for mainnet

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Set up the versions to be used
-coreth_version=${CORETH_VERSION:-'v0.1.2-0.20220713134154-aac8c98493ba'}
+coreth_version=${CORETH_VERSION:-'v0.1.2-0.20220713145751-55eb3537825b'}
 # Don't export them as they're used in the context of other calls
-axia_version=${AXIA_VERSION:-'v0.1.2-0.20220713134154-aac8c98493ba'}
+axia_version=${AXIA_VERSION:-'v0.1.2-0.20220713145751-55eb3537825b'}
